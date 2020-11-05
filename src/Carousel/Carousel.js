e72f28fdeae089f709e9c99c33623e463c74c492
yohannmartinez <yohannmartinez34@gmail.com>
2020-11-05 19:37:27 +0100

    changed style

--- src/Carousel/Carousel.js
+++ src/Carousel/Carousel.js
@@ -7,33 +7,64 @@ class Carousel extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            artistsIndex: [1, 2, 3, 4, 5],
+            artistsIndex: [1, 2, 3, 4, 5, 6,7,8,9,10,16,17,18,19,21],
             artists: [],
-            carouselIndex:0,
+            carouselIndex: 0,
+            page:0,
         }
         this.setSelectedArtist = this.setSelectedArtist.bind(this)
+        this.removePage = this.removePage.bind(this)
+        this.addPage = this.addPage.bind(this)
     }
 
     componentDidMount() {
         this.state.artistsIndex.map((artist) => {
-            axios.get(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${artist}`).then(res => { 
-                this.setState({artists : [...this.state.artists, res.data]}, ()=>{console.log(this.state.artists)}) 
+            axios.get(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${artist}`).then(res => {
+                this.setState({ artists: [...this.state.artists, res.data] })
             })
         })
     }
 
     setSelectedArtist(artistIndex) {
-        this.setState({carouselIndex: artistIndex})
+        this.setState({ carouselIndex: artistIndex })
+    }
+
+    addPage() {
+        //if this.state.page === maximum number of pages based on artists length, do nothing
+        if(this.state.page === (Math.ceil(this.state.artists.length / 5) - 1)){
+            return
+        } else {
+            this.setState({page : this.state.page + 1})
+        }
+    }
+
+    removePage() {
+        if(this.state.page === 0) {
+           return 
+        } else {
+            this.setState({page : this.state.page - 1})
+        }
     }
 
 
     render() {
         return (
-            <div className="Carousel__container" style={{marginLeft : `calc(0px - (${this.state.carouselIndex} * 10em))`}}>
-                {this.state.artists.map((artist,artistIndex)=>(
-                    <CarouselElement setSelectedArtist={this.setSelectedArtist} artist={artist} isSelected={this.state.carouselIndex === artistIndex ? true : false} artistIndex={artistIndex}/>
-                ))}
+            <div className="Carousel__globalContainer">
+                <div className="Carousel__navContainer">
+                    <span className="Carousel__carouselTitle">Popular</span>
+                    <div className="Carousel__nav">
+                        <div className="Carousel__leftnav" style={{border: this.state.page === 0 ? "1px solid white" : "1px solid black"}} onClick={()=>{this.removePage()}}>{"<"}</div>
+                        <div className="Carousel__rightnav" style={{border: this.state.page !== Math.ceil(this.state.artists.length / 5) - 1 ? "1px solid black" : "1px solid white" }} onClick={()=>{this.addPage()}}>{">"}</div>
+                    </div>
+                </div>
+                <div className="Carousel__cardsContainer" >
+                    {/* to change number of displayed cards, change the second 5 */}
+                    {this.state.artists.slice((0 + 5 * this.state.page),(5 + 5 * this.state.page)).map((artist, artistIndex) => (
+                        <CarouselElement setSelectedArtist={this.setSelectedArtist} artist={artist} isSelected={this.state.carouselIndex === artistIndex ? true : false} artistIndex={artistIndex} title={artist.name} description={artist.type}/>
+                    ))}
+                </div>
             </div>
+
         )
     }
 }
--- src/CarouselElement/CarouselElement.js
+++ src/CarouselElement/CarouselElement.js
@@ -10,21 +10,17 @@ class CarouselElement extends React.Component {
         }
     }
 
-    componentDidMount() {
-        console.log("props are ", this.props)
-    }
-
     render() {
         return (
-            <div className="CarouselElement__container"
-                onClick={()=>{this.props.setSelectedArtist(this.props.artistIndex)}}
-                style={{
-                    backgroundImage: `url(${this.props.artist.picture_xl})`,
-                    height: this.props.isSelected ? "15em" : "10em",
-                    width: this.props.isSelected ? "15em" : "10em",
-                    marginLeft: `-2em`, zIndex: this.props.isSelected ? "100" : `calc(10 - ${this.props.artistIndex})`
-                }}>
-
+            <div className="CarouselElement__globalContainer">
+                <div className="CarouselElement__container"
+                    onClick={() => { this.props.setSelectedArtist(this.props.artistIndex) }}
+                    style={{
+                        backgroundImage: `url(${this.props.artist.picture_xl})`,
+                    }}>
+                </div>
+                <div className="CarouselElement__title">{this.props.title}</div>
+                <div className="CarouselElement__description">{this.props.description}</div>
             </div>
         )
     }
